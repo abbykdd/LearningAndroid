2007fbb5f065a97f5ad44b1c7ebd0856be8c8714
Ayhan <ayqolpan.kdd@gmail.com>
2017-10-27 17:20:47 -0700

    played with buttons changing color
    yey!
    
    so much fun!
    
    thank you Rahman!
    
    you're the best!! Muaaaaax

--- app/src/main/java/com/example/rolin/testingout/MainActivity.java
+++ app/src/main/java/com/example/rolin/testingout/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.rolin.testingout;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button leftButton = findViewById(R.id.left_button);
+        final Button rightButton = findViewById(R.id.right_button);
+
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Button) view).setBackgroundColor(Color.RED);
+                rightButton.setBackgroundColor(Color.GRAY);
+                ((Button) view).setText("selected");
+                rightButton.setText("not-selected");
+                Toast.makeText(getApplicationContext(), "you clicked left button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Button) view).setBackgroundColor(Color.RED);
+                leftButton.setBackgroundColor(Color.GRAY);
+                ((Button)view).setText("selected");
+                leftButton.setText("not-selected");
+                Toast.makeText(getApplicationContext(), "you clicked right button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
